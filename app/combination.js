212ebf75909fadaf0d4f01d84742c7aec0f0e4a8
CeeDyyy <cdnoob@gmail.com>
2023-07-29 14:26:46 +0700

    change combination

--- app/combination.js
+++ app/combination.js
@@ -5,10 +5,7 @@ import React, { useRef, useState } from "react"
 export default function Combination() {
     const digits = 6
     const digitRefs = useRef([]);
-    const verifyBtn = useRef()
     const [code, setCode] = useState([])
-
-    const [allCombination, setAllCombination] = useState([])
 
     function setCodeArray(index, value) {
         var arr = [...code]
@@ -16,7 +13,7 @@ export default function Combination() {
         if (~index) {
             arr[index] = value.at(-1)
             setCode(arr)
-            value.at(-1) !== undefined && (digits > (index + 1) ? digitRefs.current[index + 1].focus() : verifyBtn.current.focus())
+            value.at(-1) !== undefined && (digits > (index + 1) ? digitRefs.current[index + 1].focus() : null)
         }
     }
 
@@ -29,51 +26,49 @@ export default function Combination() {
             setCode(arr)
         }
     }
-
-    function combination(arr) {
-        let array = arr
-        let results = [];
-
-        for (let i = 0; i < array.length - 1; i++) {
-            for (let j = i + 1; j < array.length; j++) {
-                for (let k = j + 1; k < array.length; k++) {
-                    results.push(`${array[i]} ${array[j]} ${array[k]}`);
-                }
-            }
-        }
-        // console.log(results)
-        setAllCombination(results);
-    }
     return (
         <div className="m-2 flex flex-col justify-center">
-            <div className="mt-3 flex">
+            <div className="mx-auto grid grid-cols-3 gap-3">
                 {[...Array(digits)].map((_, index) =>
-                    <div className="flex items-center" key={index}>
-                        {index !== 0 && <p className="">-</p>}
-                        <input
-                            ref={el => (digitRefs.current[index] = el)}
-                            type="number"
-                            value={code[index] ? code[index] : ""}
-                            onChange={(e) => setCodeArray(index, e.target.value)}
-                            onKeyDown={(e) => handleKeyDown(index, e.key)}
-                            onFocus={(e) => e.target.select()}
-                            placeholder="X"
-                            className="border-2 border-gray-400 bg-[#fff] placeholder-gray-400 text-center font-normal w-full py-3 rounded-[10px] [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-                        />
-                    </div>
+                    <input
+                        ref={el => (digitRefs.current[index] = el)}
+                        type="number"
+                        value={code[index] ? code[index] : ""}
+                        onChange={(e) => setCodeArray(index, e.target.value)}
+                        onKeyDown={(e) => handleKeyDown(index, e.key)}
+                        onFocus={(e) => e.target.select()}
+                        placeholder="X"
+                        className={`${index === 3 && 'order-last'} ${index === 4 && 'order-5'} ${index === 5 && 'order-4'} border-2 border-gray-400 bg-[#fff] placeholder-gray-400 text-center font-normal w-full py-3 rounded-[10px] [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none`}
+                        key={index}
+                    />
                 )}
             </div>
-            <button
-                ref={verifyBtn}
-                onClick={() => combination(code)}
-                onKeyDown={(e) => handleKeyDown(digits, e.key)}
-                className="bg-blue-300 my-4 mx-4 rounded-full p-2 text-white"
-            >
-                คำนวน
-            </button>
-            <ul>
-                {allCombination.map((d, i) => <li key={i}><span className="font-bold">{i + 1}. </span>{d}</li>)}
-            </ul>
+            <div className="mt-6 mx-auto grid grid-cols-2">
+                <div className="border-x px-4">
+                    <p>{code[0]}{code[1]}{code[2]}</p>
+                    <p>{code[0]}{code[1]}{code[3]}</p>
+                    <p>{code[0]}{code[1]}{code[4]}</p>
+                    <p>{code[0]}{code[1]}{code[5]}</p>
+                    <p>{code[1]}{code[2]}{code[3]}</p>
+                    <p>{code[1]}{code[2]}{code[4]}</p>
+                    <p>{code[1]}{code[2]}{code[5]}</p>
+                    <p>{code[2]}{code[3]}{code[4]}</p>
+                    <p>{code[2]}{code[3]}{code[5]}</p>
+                    <p>{code[2]}{code[3]}{code[0]}</p>
+                </div>
+                <div className="border-x px-4">
+                    <p>{code[3]}{code[4]}{code[5]}</p>
+                    <p>{code[3]}{code[4]}{code[0]}</p>
+                    <p>{code[3]}{code[4]}{code[1]}</p>
+                    <p>{code[4]}{code[5]}{code[0]}</p>
+                    <p>{code[4]}{code[5]}{code[1]}</p>
+                    <p>{code[4]}{code[5]}{code[2]}</p>
+                    <p>{code[5]}{code[0]}{code[2]}</p>
+                    <p>{code[5]}{code[0]}{code[3]}</p>
+                    <p>{code[5]}{code[1]}{code[3]}</p>
+                    <p>{code[0]}{code[2]}{code[4]}</p>
+                </div>
+            </div>
         </div>
     )
 }
