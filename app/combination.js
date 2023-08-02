77f3856a2e88376cc259133d562ede42e9c5dc04
CeeDyyy <cdnoob@gmail.com>
2023-08-02 14:59:40 +0700

    change text size to 3xl

--- app/combination.js
+++ app/combination.js
@@ -28,7 +28,7 @@ export default function Combination() {
     }
     return (
         <div className="m-2 flex flex-col justify-center">
-            <div className="mx-auto grid grid-cols-3">
+            <div className="mx-auto grid grid-cols-3 w-80">
                 {[...Array(digits)].map((_, index) =>
                     <input
                         ref={el => (digitRefs.current[index] = el)}
@@ -38,25 +38,25 @@ export default function Combination() {
                         onKeyDown={(e) => handleKeyDown(index, e.key)}
                         onFocus={(e) => e.target.select()}
                         placeholder="X"
-                        className={`${index === 3 && 'order-last'} ${index === 4 && 'order-5'} ${index === 5 && 'order-4'} bg-yellow-100 placeholder-gray-400 text-center font-normal w-full py-3 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none border-gray-400 ${index === 0 && 'border-r-2 border-b-2'} ${index === 1 && 'border-x-2 border-b-2'} ${index === 2 && 'border-l-2 border-b-2'} ${index === 3 && 'border-l-2 border-t-2'} ${index === 4 && 'border-x-2 border-t-2'} ${index === 5 && 'border-r-2 border-t-2'}`}
+                        className={`${index === 3 && 'order-last'} ${index === 4 && 'order-5'} ${index === 5 && 'order-4'} text-3xl bg-yellow-100 placeholder-gray-400 text-center font-normal w-full py-3 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none border-gray-400 ${index === 0 && 'border-r-2 border-b-2'} ${index === 1 && 'border-x-2 border-b-2'} ${index === 2 && 'border-l-2 border-b-2'} ${index === 3 && 'border-l-2 border-t-2'} ${index === 4 && 'border-x-2 border-t-2'} ${index === 5 && 'border-r-2 border-t-2'}`}
                         key={index}
                     />
                 )}
             </div>
-            <div className="mt-6 mx-auto grid grid-cols-4">
-                <div className="text-end">
+            <div className="mt-6 mx-auto grid grid-cols-4 h-[73vh] text-3xl">
+                <div className="text-end grid gap-5">
                     <p>1</p>
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
+                    <p className="collapse">2</p>
+                    <p className="collapse">3</p>
+                    <p className="collapse">4</p>
+                    <p className="collapse">5</p>
+                    <p className="collapse">6</p>
+                    <p className="collapse">7</p>
+                    <p className="collapse">8</p>
+                    <p className="collapse">9</p>
                     <p>10</p>
                 </div>
-                <div className="border-x-2 border-gray-400 px-4 bg-yellow-50">
+                <div className="border-x-2 border-gray-400 px-4 bg-yellow-50 grid gap-5">
                     <p>{code[0]}{code[1]}{code[2]}</p>
                     <p>{code[0]}{code[1]}{code[3]}</p>
                     <p>{code[0]}{code[1]}{code[4]}</p>
@@ -68,7 +68,7 @@ export default function Combination() {
                     <p>{code[2]}{code[3]}{code[5]}</p>
                     <p>{code[2]}{code[3]}{code[0]}</p>
                 </div>
-                <div className="border-x-2 border-gray-400 px-4 bg-yellow-50">
+                <div className="border-x-2 border-gray-400 px-4 bg-yellow-50 grid gap-5">
                     <p>{code[3]}{code[4]}{code[5]}</p>
                     <p>{code[3]}{code[4]}{code[0]}</p>
                     <p>{code[3]}{code[4]}{code[1]}</p>
@@ -80,16 +80,16 @@ export default function Combination() {
                     <p>{code[5]}{code[1]}{code[3]}</p>
                     <p>{code[0]}{code[2]}{code[4]}</p>
                 </div>
-                <div>
+                <div className="grid gap-5">
                     <p>11</p>
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
+                    <p className="collapse">2</p>
+                    <p className="collapse">3</p>
+                    <p className="collapse">4</p>
+                    <p className="collapse">5</p>
+                    <p className="collapse">6</p>
+                    <p className="collapse">7</p>
+                    <p className="collapse">8</p>
+                    <p className="collapse">9</p>
                     <p>20</p>
                 </div>
             </div>
